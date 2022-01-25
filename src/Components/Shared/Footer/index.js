5014a73d88a528e430214eefe04a3710cf87936b
huy8856 <thai.giahuy123@gmail.com>
2022-01-25 22:49:29 +0700

    Redesign footer logo and redirect link implement

--- src/Components/Shared/Footer/index.js
+++ src/Components/Shared/Footer/index.js
@@ -7,18 +7,35 @@ import {
 import "./footer.css"
 
 function Footer() {
+	const handleScrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		})
+	}
+
 	return (
 		<div className="footer">
 			<div className="facebook-icon">
-				<BsFacebook size={30} />
+				<a href="https://www.facebook.com/giahuythai/">
+					<BsFacebook size={30} />
+				</a>
 			</div>
 			<div className="discord-icon">
-				<BsDiscord size={30} />
+				<a href="https://discord.com/users/304967907505340427">
+					<BsDiscord size={30} />
+				</a>
 			</div>
 			<div className="github-icon">
-				<BsGithub size={30} />
+				<a href="https://github.com/huy232">
+					<BsGithub size={30} />
+				</a>
 			</div>
-			<div className="go-up-icon" style={{ marginRight: "50px" }}>
+			<div
+				className="go-up-icon"
+				style={{ marginRight: "50px" }}
+				onClick={handleScrollToTop}
+			>
 				<BsFileArrowUpFill size={30} />
 			</div>
 		</div>
