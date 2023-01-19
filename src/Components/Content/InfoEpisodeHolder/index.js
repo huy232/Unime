8ea5e05bcdc5aa49111b4bb1b6bf674cba379ced
huy8856 <thai.giahuy123@gmail.com>
2023-01-19 10:41:09 +0700

    Add navigation back to episode holder

--- src/Components/Content/InfoEpisodeHolder/index.js
+++ src/Components/Content/InfoEpisodeHolder/index.js
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css"
 import "swiper/css/pagination"
 import "swiper/css/navigation"
+import "./infoepisodeholder.css"
 import SwiperCore, { Pagination, Navigation } from "swiper"
 SwiperCore.use([Pagination, Navigation])
 
@@ -29,43 +30,45 @@ function InfoEpisodeHolder({
 					<Swiper
 						slidesPerView="auto"
 						className="swiper-container swiper-info"
-						navigation={false}
+						navigation={true}
 						pagination={{
 							type: "fraction",
 						}}
 					>
-						{episodeList.map((episodeChunk, i) => (
-							<SwiperSlide
-								key={i}
-								style={{
-									width: "160px",
-									display: "flex",
-									justifyContent: "center",
-									alignItems: "center",
-								}}
-							>
-								<li
-									onClick={() => {
-										setSelectedChunk(i)
+						<div className="swiper-episode-holder">
+							{episodeList.map((episodeChunk, i) => (
+								<SwiperSlide
+									key={i}
+									style={{
+										width: "160px",
+										display: "flex",
+										justifyContent: "center",
+										alignItems: "center",
 									}}
-									className="episode-chunk"
-									style={
-										selectedChunk === i
-											? {
-													color: "black",
-													backgroundColor: "white",
-													borderRadius: "8px",
-													transition: "all 0.4s linear",
-											  }
-											: {}
-									}
 								>
-									{`${episodeChunk[0].name} - ${
-										episodeChunk[episodeChunk.length - 1].name
-									}`}
-								</li>
-							</SwiperSlide>
-						))}
+									<li
+										onClick={() => {
+											setSelectedChunk(i)
+										}}
+										className="episode-chunk"
+										style={
+											selectedChunk === i
+												? {
+														color: "black",
+														backgroundColor: "white",
+														borderRadius: "8px",
+														transition: "all 0.4s linear",
+												  }
+												: {}
+										}
+									>
+										{`${episodeChunk[0].name} - ${
+											episodeChunk[episodeChunk.length - 1].name
+										}`}
+									</li>
+								</SwiperSlide>
+							))}
+						</div>
 					</Swiper>
 				)}
 			</div>
