9c061defc10d0c07454090e7c62b2b49e751685c
huy8856 <thai.giahuy123@gmail.com>
2023-01-28 09:07:12 +0700

    Change order of override boostrap to later

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 import "swiper/css/bundle"
 import "./index.css"
-import "bootstrap/dist/css/bootstrap.min.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "react-loading-skeleton/dist/skeleton.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./override.css"
 
 const root = createRoot(document.getElementById("root"))
