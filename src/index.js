653572bcd3c3727c66e6169a715afff3ba4f1c67
huy8856 <thai.giahuy123@gmail.com>
2023-01-28 09:14:11 +0700

    Change order of loading again

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
+import "./index.css"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 import "swiper/css/bundle"
-import "./index.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "react-loading-skeleton/dist/skeleton.css"
-import "bootstrap/dist/css/bootstrap.min.css"
 import "./override.css"
 
 const root = createRoot(document.getElementById("root"))
