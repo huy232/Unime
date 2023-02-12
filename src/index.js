cd0fd06e88d7ab8476326be44ae42c4be483ebb4
huy8856 <thai.giahuy123@gmail.com>
2023-02-12 16:10:58 +0700

    Config scripts again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-// import "./index.css"
-import "./tailwind.css"
+import "./index.css"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 import "swiper/css/bundle"
