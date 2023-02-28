d15b524355de773b1eb51f563c212d850018c3db
huy8856 <thai.giahuy123@gmail.com>
2023-02-28 10:34:55 +0700

    Try to remove GENERATE MAP

--- src/Utilities/debugger.js
+++ src/Utilities/debugger.js
@@ -1,1 +1,131 @@
+// const _0xbce509 = (function () {
+// 	let _0x389972 = !![]
+// 	return function (_0x135268, _0x1b8327) {
+// 		const _0x32f2f8 = _0x389972
+// 			? function () {
+// 					if (_0x1b8327) {
+// 						const _0x1ac4f6 = _0x1b8327["apply"](_0x135268, arguments)
+// 						_0x1b8327 = null
+// 						return _0x1ac4f6
+// 					}
+// 			  }
+// 			: function () {}
+// 		_0x389972 = ![]
+// 		return _0x32f2f8
+// 	}
+// })()
+// ;(function () {
+// 	_0xbce509(this, function () {
+// 		const _0x1249a1 = new RegExp("function\x20*\x5c(\x20*\x5c)")
+// 		const _0x4bf0c0 = new RegExp(
+// 			"\x5c+\x5c+\x20*(?:[a-zA-Z_$][0-9a-zA-Z_$]*)",
+// 			"i"
+// 		)
+// 		const _0x5b78ae = _0x2f5af1("init")
+// 		if (
+// 			!_0x1249a1["test"](_0x5b78ae + "chain") ||
+// 			!_0x4bf0c0["test"](_0x5b78ae + "input")
+// 		) {
+// 			_0x5b78ae("0")
+// 		} else {
+// 			_0x2f5af1()
+// 		}
+// 	})()
+// })()
+// const _0x54b357 = (function () {
+// 	let _0x43c940 = !![]
+// 	return function (_0x25942f, _0x5a2471) {
+// 		const _0x4da413 = _0x43c940
+// 			? function () {
+// 					if (_0x5a2471) {
+// 						const _0x28f7dd = _0x5a2471["apply"](_0x25942f, arguments)
+// 						_0x5a2471 = null
+// 						return _0x28f7dd
+// 					}
+// 			  }
+// 			: function () {}
+// 		_0x43c940 = ![]
+// 		return _0x4da413
+// 	}
+// })()
+// const _0xbfbceb = _0x54b357(this, function () {
+// 	const _0x4d78a4 = function () {
+// 		let _0x4c1b01
+// 		try {
+// 			_0x4c1b01 = Function(
+// 				"return\x20(function()\x20" +
+// 					"{}.constructor(\x22return\x20this\x22)(\x20)" +
+// 					");"
+// 			)()
+// 		} catch (_0x282f20) {
+// 			_0x4c1b01 = window
+// 		}
+// 		return _0x4c1b01
+// 	}
+// 	const _0x3377dc = _0x4d78a4()
+// 	const _0x2f5c76 = (_0x3377dc["console"] = _0x3377dc["console"] || {})
+// 	const _0x4f0c04 = [
+// 		"log",
+// 		"warn",
+// 		"info",
+// 		"error",
+// 		"exception",
+// 		"table",
+// 		"trace",
+// 	]
+// 	for (let _0x1326b3 = 0x0; _0x1326b3 < _0x4f0c04["length"]; _0x1326b3++) {
+// 		const _0x62dabf = _0x54b357["constructor"]["prototype"]["bind"](_0x54b357)
+// 		const _0x4fa578 = _0x4f0c04[_0x1326b3]
+// 		const _0xc91cd5 = _0x2f5c76[_0x4fa578] || _0x62dabf
+// 		_0x62dabf["__proto__"] = _0x54b357["bind"](_0x54b357)
+// 		_0x62dabf["toString"] = _0xc91cd5["toString"]["bind"](_0xc91cd5)
+// 		_0x2f5c76[_0x4fa578] = _0x62dabf
+// 	}
+// })
+// _0xbfbceb()
+// let div = document["createElement"]("div")
+// let loop = setInterval(() => {
+// 	debugger
+// })
+// Object["defineProperty"](div, "id", {
+// 	get: () => {
+// 		clearInterval(loop)
+// 		alert("Dev\x20Tools\x20detected!")
+// 	},
+// })
+// function _0x2f5af1(_0x4786ff) {
+// 	function _0x449579(_0x183d54) {
+// 		if (typeof _0x183d54 === "string") {
+// 			return function (_0x5bee06) {}
+// 				["constructor"]("while\x20(true)\x20{}")
+// 				["apply"]("counter")
+// 		} else {
+// 			if (
+// 				("" + _0x183d54 / _0x183d54)["length"] !== 0x1 ||
+// 				_0x183d54 % 0x14 === 0x0
+// 			) {
+// 				;(function () {
+// 					return !![]
+// 				}
+// 					["constructor"]("debu" + "gger")
+// 					["call"]("action"))
+// 			} else {
+// 				;(function () {
+// 					return ![]
+// 				}
+// 					["constructor"]("debu" + "gger")
+// 					["apply"]("stateObject"))
+// 			}
+// 		}
+// 		_0x449579(++_0x183d54)
+// 	}
+// 	try {
+// 		if (_0x4786ff) {
+// 			return _0x449579
+// 		} else {
+// 			_0x449579(0x0)
+// 		}
+// 	} catch (_0x2c2c15) {}
+// }
 
+// export const DEBUGGER = _0xbce509
