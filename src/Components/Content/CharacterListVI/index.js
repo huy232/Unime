9fe138b8058c9c33df47deb6d90ffb391127ea4e
Huy <thai.giahuy123@gmail.com>
2023-03-02 09:35:21 +0700

    On progress create Character list for VI info

---
+++ src/Components/Content/CharacterListVI/index.js
@@ -0,0 +1,18 @@
+function CharacterListVI({ characters, loading }) {
+	console.log(characters)
+	return (
+		<>
+			{/* {!loading && 
+        <>
+            <h4>DÀN NHÂN VẬT</h4>
+        <div className="">
+            {characters.map((chacter) => )}
+        </div>
+        
+        </>
+    } */}
+		</>
+	)
+}
+
+export default CharacterListVI
