06cb6d487b8ddc3579e866f675218711e0d4c64e
huy8856 <thai.giahuy123@gmail.com>
2023-03-29 21:30:28 +0700

    Try to fix unknown case in RECOMMEND

--- src/Components/Content/RecommendENG/index.js
+++ src/Components/Content/RecommendENG/index.js
@@ -8,6 +8,7 @@ import "swiper/css/pagination"
 
 import "./recommend.css"
 import { Lazy } from "swiper"
+import { COLORLIST } from "../../../constants"
 
 function RecommendENG({ recommend, setLoading }) {
 	return (
@@ -27,57 +28,76 @@ function RecommendENG({ recommend, setLoading }) {
 					lazy={true}
 					preloadImages={false}
 				>
-					{recommend.map((anime) => (
-						<SwiperSlide key={anime.id}>
-							<Link
-								to={`/eng/info/${anime.id}`}
-								title={
-									anime.title.english ||
-									anime.title.romaji ||
-									anime.title.native ||
-									anime.title.userPreferred
-								}
-								onClick={() => setLoading(true)}
-								aria-label={
-									anime.title.english ||
-									anime.title.romaji ||
-									anime.title.native ||
-									anime.userPreferred
-								}
-							>
-								<div className="group recommend-anime-holder select-none cursor-pointer">
-									<div className="recommend-anime-image aspect-[2/3] group-hover:opacity-80 duration-200 ease-in-out relative">
-										<img
-											className="object-fill object-center w-full h-full group-hover:scale-90 duration-500 linear absolute"
-											src={anime.image}
-											alt={
-												anime.title.english ||
-												anime.title.romaji ||
-												anime.title.native ||
-												anime.title.userPreferred
-											}
-											loading="lazy"
-										/>
-										<div className="text-white absolute right-0 bg-[#282828cc] p-[6px]">
-											{anime.rating}%{" "}
-											<FontAwesomeIcon
-												icon={faHeart}
-												className="text-red-500"
-											/>
+					{recommend.map(
+						(anime, i) =>
+							anime.status !== "Unknown" && (
+								<SwiperSlide key={i}>
+									<Link
+										to={`/eng/info/${anime.id}`}
+										title={
+											anime.title.english ||
+											anime.title.romaji ||
+											anime.title.native ||
+											anime.title.userPreferred
+										}
+										onClick={() => setLoading(true)}
+										aria-label={
+											anime.title.english ||
+											anime.title.romaji ||
+											anime.title.native ||
+											anime.userPreferred
+										}
+									>
+										<div className="group recommend-anime-holder select-none cursor-pointer">
+											<div className="recommend-anime-image aspect-[2/3] group-hover:opacity-80 duration-200 ease-in-out relative">
+												<img
+													className="object-fill object-center w-full h-full duration-500 linear absolute"
+													src={anime.image}
+													alt={
+														anime.title.english ||
+														anime.title.romaji ||
+														anime.title.native ||
+														anime.title.userPreferred
+													}
+													loading="lazy"
+												/>
+												<div className="text-white absolute right-0 bg-[#0d0d0d]/[0.8] p-[6px]">
+													{anime.rating}%{" "}
+													<FontAwesomeIcon
+														icon={faHeart}
+														className="text-red-500"
+													/>
+												</div>
+												<div className="text-[#fffc] absolute bottom-0 text-sm w-full p-[4px] bg-[#0d0d0d]/[0.8]">
+													<div className="flex">
+														<p>{anime.episodes}. EP</p>
+														<p className="ml-auto border-2 rounded px-[4px]">
+															{anime.type}
+														</p>
+													</div>
+												</div>
+											</div>
+											<div className="recommend-anime-title h-[60px]">
+												<p
+													className="line-clamp-2 font-extrabold"
+													style={{ color: COLORLIST[i] || "#fffc" }}
+												>
+													{anime.title.english ||
+														anime.title.romaji ||
+														anime.title.native ||
+														anime.title.userPreferred}
+												</p>
+											</div>
+											<div className="text-[#fffc] text-sm inline-block p-[4px]">
+												<p className="border-2 rounded px-[4px]">
+													{anime.status}
+												</p>
+											</div>
 										</div>
-									</div>
-									<div className="recommend-anime-title">
-										<p className="text-amber-300 line-clamp-2 font-medium">
-											{anime.title.english ||
-												anime.title.romaji ||
-												anime.title.native ||
-												anime.title.userPreferred}
-										</p>
-									</div>
-								</div>
-							</Link>
-						</SwiperSlide>
-					))}
+									</Link>
+								</SwiperSlide>
+							)
+					)}
 				</Swiper>
 			</div>
 		</>
