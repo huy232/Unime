6aa1d33a4667e6b42570829ffe3e05922ac1faa3
Huy <thai.giahuy123@gmail.com>
2023-04-23 09:03:43 +0700

    Save choosing state of ENG site to local storage

--- src/Components/Content/ContentToggleENG/index.js
+++ src/Components/Content/ContentToggleENG/index.js
@@ -3,29 +3,33 @@ import { useNavigate } from "react-router-dom"
 
 function ContentToggleENG() {
 	const navigate = useNavigate()
-	const [contentToggle, setContentToggle] = useState(true)
+	const [contentToggle, setContentToggle] = useState(
+		JSON.parse(localStorage.getItem("unime-eng-navigate"))
+	)
 	const buttonClassName = `rounded-sm bg-white/20 hover:opacity-80 duration-500 ease-in-out font-bold px-[6px]`
 	return (
 		<div>
 			{contentToggle ? (
 				<button
 					onClick={() => {
+						localStorage.setItem("unime-eng-navigate", JSON.stringify(false))
 						setContentToggle(false)
-						navigate("/eng/manga")
-					}}
-					className={buttonClassName}
-				>
-					Anime
-				</button>
-			) : (
-				<button
-					onClick={() => {
-						setContentToggle(true)
 						navigate("/eng")
 					}}
 					className={buttonClassName}
 				>
 					Manga
+				</button>
+			) : (
+				<button
+					onClick={() => {
+						localStorage.setItem("unime-eng-navigate", JSON.stringify(true))
+						setContentToggle(true)
+						navigate("/eng/manga")
+					}}
+					className={buttonClassName}
+				>
+					Anime
 				</button>
 			)}
 		</div>
