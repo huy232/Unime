b111480f19a51121781dc21dc7f0e2d11ab6d1e6
huy8856 <thai.giahuy123@gmail.com>
2023-04-27 00:00:53 +0700

    Center comment title

--- src/Components/Content/CommentSection/index.js
+++ src/Components/Content/CommentSection/index.js
@@ -17,8 +17,8 @@ function CommentSection({
 		language: language,
 	}
 	return (
-		<div className="article-container px-4 md:px-12 lg:px-20 xl:px-28 2xl:px-36">
-			<h4 className="font-black">{headingTitle}</h4>
+		<div className="article-container px-4 md:px-12 lg:px-20 xl:px-28 2xl:px-36 my-2">
+			<h4 className="font-black max-md:text-center">{headingTitle}</h4>
 			<p className="inline-block text-white/50 border-l-[6px] border-white/70 p-[4px] bg-white/10 text-sm my-[8px]">
 				{language === "vi" && (
 					<>
