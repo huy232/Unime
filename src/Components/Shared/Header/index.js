c0d2380f61ad48e5f25fff073771903b4cc0f9b6
huy8856 <thai.giahuy123@gmail.com>
2023-05-01 19:58:38 +0700

    Remove console.log

--- src/Components/Shared/Header/index.js
+++ src/Components/Shared/Header/index.js
@@ -2,16 +2,10 @@ import React from "react"
 import { useAuth } from "../../../Contexts/auth"
 import HeaderVI from "../HeaderVI"
 import HeaderENG from "../HeaderENG"
-import { useLocation } from "react-router-dom"
 
 function Header() {
-	const pathname = useLocation()
 	const { language } = useAuth()
-	return (
-		<>
-			{pathname.pathname.split("/")[1] !== "eng" ? <HeaderVI /> : <HeaderENG />}
-		</>
-	)
+	return <>{language === "eng" ? <HeaderENG /> : <HeaderVI />}</>
 }
 
 export default Header
