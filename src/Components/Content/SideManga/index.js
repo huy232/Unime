951ad277855f29e9c7e7026c7dcedd8b5956426e
Huy <thai.giahuy123@gmail.com>
2023-05-04 09:34:12 +0700

    Add a chapter list heading

--- src/Components/Content/SideManga/index.js
+++ src/Components/Content/SideManga/index.js
@@ -7,43 +7,50 @@ function SideManga({ chapters, provider, mangaID, chapterID }) {
 	const scrollToRef = useRef(null)
 	const [toggleSidebar, setToggleSidebar] = useState(false)
 	return (
-		<div className="fixed top-[40px] left-0 flex">
-			<ul
-				className={`${
-					toggleSidebar ? "w-[296px]" : "w-[0px] left-[-330px]"
-				} h-[calc(100vh-40px)] overflow-y-scroll relative duration-200 ease-in-out bg-black`}
-			>
-				{chapters.reverse().map((item) => (
-					<Link
-						to={`/eng/manga-read?mangaID=${mangaID}&chapterID=${item.id}&provider=${provider}`}
-						className={`text-white hover:opacity-80 duration-200`}
-						key={item.id}
-					>
-						<li
-							className={`py-[6px] line-clamp-2 ${
-								item.id === chapterID ? "bg-orange-600" : ""
-							}`}
-							ref={scrollToRef}
+		<div
+			className={`fixed top-[40px] z-50 ${
+				toggleSidebar ? "left-0" : "left-[-320px]"
+			} duration-200 ease-in-out`}
+		>
+			<h2 className="h-[40px]">CHAPTER LIST</h2>
+			<div className="flex">
+				<ul
+					className={`h-[calc(100vh-80px)] overflow-y-scroll relative bg-black w-[320px]`}
+				>
+					{chapters.reverse().map((item) => (
+						<Link
+							to={`/eng/manga-read?mangaID=${mangaID}&chapterID=${item.id}&provider=${provider}`}
+							className={`text-white hover:opacity-80 duration-200`}
+							key={item.id}
 						>
-							{item.title}
-						</li>
-					</Link>
-				))}
-			</ul>
-			<div className="ml-[4px]">
-				{toggleSidebar ? (
-					<FontAwesomeIcon
-						className="cursor-pointer rounded-full bg-gray-700 w-[20px] h-[20px] p-1"
-						icon={faArrowLeft}
-						onClick={() => setToggleSidebar(!toggleSidebar)}
-					/>
-				) : (
-					<FontAwesomeIcon
-						className="cursor-pointer rounded-full bg-gray-700 w-[20px] h-[20px] p-1"
-						icon={faArrowRight}
-						onClick={() => setToggleSidebar(!toggleSidebar)}
-					/>
-				)}
+							<li
+								className={`py-[6px] line-clamp-2 ${
+									item.id === chapterID
+										? "bg-orange-600"
+										: "odd:bg-[#0D0D0D] even:bg-[#272727]"
+								}`}
+								ref={scrollToRef}
+							>
+								{item.title}
+							</li>
+						</Link>
+					))}
+				</ul>
+				<div className="ml-[4px] mt-[8px]">
+					{toggleSidebar ? (
+						<FontAwesomeIcon
+							className="cursor-pointer rounded-full bg-gray-700 w-[20px] h-[20px] p-1"
+							icon={faArrowLeft}
+							onClick={() => setToggleSidebar(!toggleSidebar)}
+						/>
+					) : (
+						<FontAwesomeIcon
+							className="cursor-pointer rounded-full bg-gray-700 w-[20px] h-[20px] p-1"
+							icon={faArrowRight}
+							onClick={() => setToggleSidebar(!toggleSidebar)}
+						/>
+					)}
+				</div>
 			</div>
 		</div>
 	)
