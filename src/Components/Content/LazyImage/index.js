ca4b90dc5afeaa65be56b04c010d7830308f683b
huy8856 <thai.giahuy123@gmail.com>
2023-06-17 08:33:06 +0700

    Replace to pure image url of each chapters

--- src/Components/Content/LazyImage/index.js
+++ src/Components/Content/LazyImage/index.js
@@ -1,19 +1,12 @@
 import React from "react"
 import blackBackground from "../../../Utilities/img/black.webp"
 import { LazyLoadImage } from "react-lazy-load-image-component"
-import { CONSUMET_CORS } from "../../../constants"
 function LazyImage({ page, provider }) {
 	return (
 		<>
 			<LazyLoadImage
 				className="aspect-[2/3]"
-				src={
-					provider === "mangareader"
-						? page.img
-						: provider === "mangadex"
-						? `${CONSUMET_CORS}url=${page.img}&referer=https://mangadex.org`
-						: `${CONSUMET_CORS}url=${page.img}&referer=${page?.headerForImage?.Referer}`
-				}
+				src={page.img}
 				alt={page.title}
 				placeholderSrc={blackBackground}
 				effect={"opacity"}
