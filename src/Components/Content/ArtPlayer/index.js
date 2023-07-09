00c6d825e006cd5d4b11302cc8801f29938f1bb4
huy8856 <thai.giahuy123@gmail.com>
2023-07-09 19:56:23 +0700

    Add subtitle for mp4 as well

--- src/Components/Content/ArtPlayer/index.js
+++ src/Components/Content/ArtPlayer/index.js
@@ -24,6 +24,36 @@ function ArtPlayer({
 				plugins: [artplayerPluginControl()],
 				settings: [
 					{
+						html: "Subtitle",
+						tooltip:
+							subtitles.find((sub) => sub.html.split(" ")[1] === selectedSub)
+								?.html || "",
+						icon: `<svg width="24" height="24" style="fill: #fffc" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 96C0 60.7 28.7 32 64 32H512c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM200 208c14.2 0 27 6.1 35.8 16c8.8 9.9 24 10.7 33.9 1.9s10.7-24 1.9-33.9c-17.5-19.6-43.1-32-71.5-32c-53 0-96 43-96 96s43 96 96 96c28.4 0 54-12.4 71.5-32c8.8-9.9 8-25-1.9-33.9s-25-8-33.9 1.9c-8.8 9.9-21.6 16-35.8 16c-26.5 0-48-21.5-48-48s21.5-48 48-48zm144 48c0-26.5 21.5-48 48-48c14.2 0 27 6.1 35.8 16c8.8 9.9 24 10.7 33.9 1.9s10.7-24 1.9-33.9c-17.5-19.6-43.1-32-71.5-32c-53 0-96 43-96 96s43 96 96 96c28.4 0 54-12.4 71.5-32c8.8-9.9 8-25-1.9-33.9s-25-8-33.9 1.9c-8.8 9.9-21.6 16-35.8 16c-26.5 0-48-21.5-48-48z"/></svg>`,
+						selector: [
+							{
+								html: "Display",
+								tooltip: "Show",
+								switch: true,
+								onSwitch: function (item) {
+									item.tooltip = item.switch ? "Hide" : "Show"
+									art.subtitle.show = !item.switch
+									return !item.switch
+								},
+							},
+							...subtitles,
+						],
+						onSelect: function (item) {
+							localStorage.setItem(
+								"artplayer-language",
+								item.html.split(" ")[1]
+							)
+							art.subtitle.switch(item.url, {
+								name: item.html,
+							})
+							return item.html
+						},
+					},
+					{
 						html: "Quality",
 						icon: `<svg width="24" height="24" style="fill: #fffc" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm-6 336H54a6 6 0 0 1-6-6V118a6 6 0 0 1 6-6h404a6 6 0 0 1 6 6v276a6 6 0 0 1-6 6zM128 152c-22.091 0-40 17.909-40 40s17.909 40 40 40 40-17.909 40-40-17.909-40-40-40zM96 352h320v-80l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L192 304l-39.515-39.515c-4.686-4.686-12.284-4.686-16.971 0L96 304v48z"/></svg>`,
 						tooltip: videoUrl[0].html,
