11ab0c5e3a3e02f52e0e30b6d0ce5947ce1ff6c9
huy8856 <thai.giahuy123@gmail.com>
2024-01-05 02:10:21 +0700

    Update missing genres ENG

--- src/Components/Content/GenresENG/index.js
+++ src/Components/Content/GenresENG/index.js
@@ -44,7 +44,7 @@ function GenresENG() {
 				<div>
 					{ENG_GENRES.map((genre, i) => (
 						<Link
-							to={`/anime/${genre}`}
+							to={`/eng/anime/${genre}`}
 							key={genre}
 							aria-label={genre}
 							style={{ background: `none`, transition: "0.5s" }}
@@ -60,7 +60,7 @@ function GenresENG() {
 				<div>
 					{ENG_GENRES.map((genre, i) => (
 						<Link
-							to={`/anime/${genre}`}
+							to={`/eng/anime/${genre}`}
 							key={genre}
 							aria-label={genre}
 							style={{ background: `none`, transition: "0.5s" }}
